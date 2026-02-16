34d1fd293da2f90be24a5dcd097a1415f0611a38
Mateo Barrera <mbarrera9@my.bcit.ca>
2026-02-16 12:08:56 -0800

    Updated index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const db = require('./portfolio-db.js');
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
-app.set('views', './views');
-app.use(express.static('public'));
-app.use('/media', express.static('media'));
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/media', express.static(path.join(__dirname, 'media')));
 
 app.get('/', (req, res) => {
     try {
-        res.render('./pages/index', {
+        res.render('pages/index', {
             projects: db.getProjects(),
             about: db.getAbout(),
             img: db.getImg(),
@@ -18,7 +19,7 @@ app.get('/', (req, res) => {
         });
     } catch (err) {
         console.error("Error in: ", err);
-        res.status(500).render('./pages/error', {
+        res.status(500).render('pages/error', {
             error: 500,
             message: "Internal server error"
         });
@@ -30,19 +31,19 @@ app.get('/project/:title', (req, res) => {
         const projectTitle = req.params.title;
         const project = db.searchProject(projectTitle);
         if (project) {
-            res.render('./pages/project', {
+            res.render('pages/project', {
                 project: project
             });
         } else {
             console.error("Project not found");
-            res.status(404).render('./pages/error', {
+            res.status(404).render('pages/error', {
                 error: 404,
                 message: "Project not found"
             });
         }
     } catch (err) {
         console.error("Error in: ", err);
-        res.status(500).render('./pages/error', {
+        res.status(500).render('pages/error', {
             error: 500,
             message: "Internal server error"
         });
@@ -51,7 +52,7 @@ app.get('/project/:title', (req, res) => {
 
 app.use((req, res) => {
     console.error("Page not found");
-    res.status(404).render('./pages/error', {
+    res.status(404).render('pages/error', {
         error: 404,
         message: "Page not found"
     });
